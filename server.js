aa95488da2440d55f66bcf3d142928bb9f1e4131
zachgarnier <zacharie.garnier@gmail.com>
2025-07-04 10:57:03 -0400

    new server

--- server.js
+++ server.js
@@ -1,42 +1,70 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const fs = require('fs');
-const cors = require('cors');
+const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(express.static('public'));
+// Middleware pour parser le JSON du corps des requêtes
+app.use(express.json());
 
-// GET ingredients
+// Servir les fichiers statiques depuis /public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// === ROUTES API ===
+
+// Lire les ingrédients
 app.get('/api/ingredients', (req, res) => {
-  fs.readFile('./data/ingredients.json', 'utf8', (err, data) => {
-    if (err) return res.status(500).send('Erreur lecture');
-    res.json(JSON.parse(data || '[]'));
+  const filePath = path.join(__dirname, 'data', 'ingredients.json');
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) return res.status(500).send('Erreur lecture fichier');
+    res.json(JSON.parse(data));
   });
 });
 
-// POST ingredients
+// Écrire les ingrédients
 app.post('/api/ingredients', (req, res) => {
-  fs.writeFile('./data/ingredients.json', JSON.stringify(req.body, null, 2), (err) => {
-    if (err) return res.status(500).send('Erreur écriture');
+  const filePath = path.join(__dirname, 'data', 'ingredients.json');
+  fs.writeFile(filePath, JSON.stringify(req.body, null, 2), 'utf8', (err) => {
+    if (err) return res.status(500).send('Erreur écriture fichier');
     res.sendStatus(200);
   });
 });
 
-// même chose pour recipes :
+// Pareil pour les recettes
 app.get('/api/recipes', (req, res) => {
-  fs.readFile('./data/recipes.json', 'utf8', (err, data) => {
-    if (err) return res.status(500).send('Erreur lecture');
-    res.json(JSON.parse(data || '[]'));
+  const filePath = path.join(__dirname, 'data', 'recipes.json');
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) return res.status(500).send('Erreur lecture fichier');
+    res.json(JSON.parse(data));
   });
 });
 
 app.post('/api/recipes', (req, res) => {
-  fs.writeFile('./data/recipes.json', JSON.stringify(req.body, null, 2), (err) => {
-    if (err) return res.status(500).send('Erreur écriture');
+  const filePath = path.join(__dirname, 'data', 'recipes.json');
+  fs.writeFile(filePath, JSON.stringify(req.body, null, 2), 'utf8', (err) => {
+    if (err) return res.status(500).send('Erreur écriture fichier');
     res.sendStatus(200);
   });
 });
 
-app.listen(3000, () => console.log('Serveur sur http://localhost:3000'));
+// Pareil pour les voyages
+app.get('/api/voyages', (req, res) => {
+  const filePath = path.join(__dirname, 'data', 'voyages.json');
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) return res.status(500).send('Erreur lecture fichier');
+    res.json(JSON.parse(data));
+  });
+});
+
+app.post('/api/voyages', (req, res) => {
+  const filePath = path.join(__dirname, 'data', 'voyages.json');
+  fs.writeFile(filePath, JSON.stringify(req.body, null, 2), 'utf8', (err) => {
+    if (err) return res.status(500).send('Erreur écriture fichier');
+    res.sendStatus(200);
+  });
+});
+
+// Démarrage du serveur
+app.listen(PORT, () => {
+  console.log(`Serveur lancé sur http://localhost:${PORT}`);
+});
