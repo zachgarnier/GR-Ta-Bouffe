aba07dc3b287abded2a9347d942ed694c776072b
zachgarnier <zacharie.garnier@gmail.com>
2025-07-04 11:12:51 -0400

    new server

--- server.js
+++ server.js
@@ -11,54 +11,48 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // === ROUTES API ===
+const getFilePath = (filename) => path.join(__dirname, 'data', filename);
 
-// Lire les ingrédients
+// Ingrédients
 app.get('/api/ingredients', (req, res) => {
-  const filePath = path.join(__dirname, 'data', 'ingredients.json');
-  fs.readFile(filePath, 'utf8', (err, data) => {
+  fs.readFile(getFilePath('ingredients.json'), 'utf8', (err, data) => {
     if (err) return res.status(500).send('Erreur lecture fichier');
-    res.json(JSON.parse(data));
+    res.json(JSON.parse(data || '[]'));
   });
 });
 
-// Écrire les ingrédients
 app.post('/api/ingredients', (req, res) => {
-  const filePath = path.join(__dirname, 'data', 'ingredients.json');
-  fs.writeFile(filePath, JSON.stringify(req.body, null, 2), 'utf8', (err) => {
+  fs.writeFile(getFilePath('ingredients.json'), JSON.stringify(req.body, null, 2), 'utf8', (err) => {
     if (err) return res.status(500).send('Erreur écriture fichier');
     res.sendStatus(200);
   });
 });
 
-// Pareil pour les recettes
+// Recettes
 app.get('/api/recipes', (req, res) => {
-  const filePath = path.join(__dirname, 'data', 'recipes.json');
-  fs.readFile(filePath, 'utf8', (err, data) => {
+  fs.readFile(getFilePath('recipes.json'), 'utf8', (err, data) => {
     if (err) return res.status(500).send('Erreur lecture fichier');
-    res.json(JSON.parse(data));
+    res.json(JSON.parse(data || '[]'));
   });
 });
 
 app.post('/api/recipes', (req, res) => {
-  const filePath = path.join(__dirname, 'data', 'recipes.json');
-  fs.writeFile(filePath, JSON.stringify(req.body, null, 2), 'utf8', (err) => {
+  fs.writeFile(getFilePath('recipes.json'), JSON.stringify(req.body, null, 2), 'utf8', (err) => {
     if (err) return res.status(500).send('Erreur écriture fichier');
     res.sendStatus(200);
   });
 });
 
-// Pareil pour les voyages
+// Voyages
 app.get('/api/voyages', (req, res) => {
-  const filePath = path.join(__dirname, 'data', 'voyages.json');
-  fs.readFile(filePath, 'utf8', (err, data) => {
+  fs.readFile(getFilePath('voyages.json'), 'utf8', (err, data) => {
     if (err) return res.status(500).send('Erreur lecture fichier');
-    res.json(JSON.parse(data));
+    res.json(JSON.parse(data || '[]'));
   });
 });
 
 app.post('/api/voyages', (req, res) => {
-  const filePath = path.join(__dirname, 'data', 'voyages.json');
-  fs.writeFile(filePath, JSON.stringify(req.body, null, 2), 'utf8', (err) => {
+  fs.writeFile(getFilePath('voyages.json'), JSON.stringify(req.body, null, 2), 'utf8', (err) => {
     if (err) return res.status(500).send('Erreur écriture fichier');
     res.sendStatus(200);
   });
@@ -66,5 +60,5 @@ app.post('/api/voyages', (req, res) => {
 
 // Démarrage du serveur
 app.listen(PORT, () => {
-  console.log(`Serveur lancé sur http://localhost:${PORT}`);
+  console.log(`✅ Serveur lancé sur http://localhost:${PORT}`);
 });
